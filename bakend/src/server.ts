37ec7df2da5cb08809473dde3aa9c955bd248e8b
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2025-08-22 09:08:12 -0300

    server on

---
+++ bakend/src/server.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express()
+app.use(express.json())
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hello, Word!')
+})
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+    console.log(`Server is ruinning on http://localhost:${port}`)
+})
