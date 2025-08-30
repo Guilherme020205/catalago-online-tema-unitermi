9d6642d38430f32c61189c8cd15a105b090187f3
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2025-08-29 21:57:16 -0300

    Update index.tsx

--- site-catalago/src/components/product/index.tsx
+++ site-catalago/src/components/product/index.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 
 interface Product {
-  id: string; // importante
+  id: string;
   img: string;
   name: string;
 }
@@ -10,12 +10,12 @@ function BoxProduct({ id, img, name }: Product) {
   const navigate = useNavigate();
 
   const goToProduct = () => {
-    navigate(`/produto/${id}`, { state: { id } });  
+    navigate(`/produto/${id}`, { state: { id } });
   };
 
   return (
     <div
-      onClick={goToProduct} // clique navega
+      onClick={goToProduct}
       className="flex flex-col items-center  h-52  w-60 p-4 rounded-2xl shadow-[2px_2px_7px_-4px_#ff0000] hover:shadow-[-4px_-4px_10px_-4px_#ff0000] duration-700 transition-all cursor-pointer select-none"
     >
       <img src={img} alt={name} className="w-36" />
