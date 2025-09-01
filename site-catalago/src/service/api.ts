7c09d6f6dc217cdd3c2cbcbf17e55b4ba4e8aa50
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2025-09-01 11:23:53 -0300

    Update api.ts

--- site-catalago/src/service/api.ts
+++ site-catalago/src/service/api.ts
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  // baseURL: 'http://localhost:3000',
+  baseURL: 'http://10.0.0.103:3000/',
 });
 
 api.interceptors.request.use((config) => {
