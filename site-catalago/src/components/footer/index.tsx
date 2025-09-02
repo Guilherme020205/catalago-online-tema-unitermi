bf20901fd35777e745ffc79cf9a13fd8cd259732
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2025-09-02 10:48:25 -0300

    Update index.tsx

--- site-catalago/src/components/footer/index.tsx
+++ site-catalago/src/components/footer/index.tsx
@@ -2,10 +2,10 @@ import logo from '../../assets/KwGSNasQ.png'
 function Footer() {
   return (
     <div className="w-full h-60 flex flex-col mt-20 bg-web-red justify-center items-center">
-      <img src={logo} alt="logo" className='w-80 mb-10'/>
-      <span className="flex flex-col items-center text-white">
+      <img src={logo} alt="logo" className='w-52 lg:w-80 mb-10'/>
+      <span className="flex flex-col items-center text-white text-xs">
         <strong>ONDE ESTAMOS:</strong>
-        <p className='underline'>
+        <p className='underline mx-10 text-center'>
           Rua Juvêncio Rodrigues, 290 – Costa do Morro – Paulo Lopes/SC – CEP:
           88490-000
         </p>
