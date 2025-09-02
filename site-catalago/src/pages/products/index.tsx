2b572e452b287d74e2099f8c1ba6b9cbc35b351e
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2025-09-02 12:15:41 -0300

    menu

--- site-catalago/src/pages/products/index.tsx
+++ site-catalago/src/pages/products/index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { api } from "../../service/api";
 import BoxProduct from "../../components/product";
 import LoadingIcon from "../../components/loadings/loadingIcon";
-import LoadingPulse from "../../components/loadings/loadingPulse";
+import Menu from "./components/menu";
 
 function ScreenProducts() {
   const [categorys, setCategorys] = useState<any[]>([]);
@@ -62,75 +62,62 @@ function ScreenProducts() {
   }, [page]);
 
   return (
-    <div>
-      <div className="flex justify-center bg-web-pink min-h-14 ">
-        {loadingCategories ? (
-          <LoadingPulse />
-        ) : (
-          <div className="flex justify-center">
-            {categorys.map((category: any) => (
+  <div className="flex">
+    <Menu
+      categorys={categorys}
+      selectedCategory={selectedCategory}
+      setSelectedCategory={setSelectedCategory}
+      loadingCategories={loadingCategories}
+    />
+
+    <div className="flex-1 mr-10 mt-10 md:mt-0">
+        <div className="flex justify-center mt-12 min-h-[200px]">
+          {loadingProducts ? (
+            <LoadingIcon />
+          ) : products.length === 0 ? (
+            <p className="text-gray-500 text-lg">
+              Nenhum produto encontrado nessa categoria.
+            </p>
+          ) : (
+            <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-20">
+              {products.map((product: any) => (
+                <BoxProduct
+                  key={product.id}
+                  id={product.id}
+                  img={product.Image}
+                  name={product.ProductLine.name}
+                />
+              ))}
+            </ul>
+          )}
+        </div>
+
+        <div className="flex flex-row justify-center items-center mt-10 gap-4">
+          {products.length > 0 && (
+            <div>
               <button
-                key={category.id}
-                onClick={() => setSelectedCategory(category.id)}
-                className={`px-5 py-0 cursor-pointer transition-all duration-700
-                ${
-                  selectedCategory === category.id
-                    ? "bg-white text-web-red text-[20px]"
-                    : "hover:bg-white hover:text-web-red hover:text-[20px]"
-                }`}
+                disabled={page === 1}
+                onClick={() => setPage(page - 1)}
+                className="px-4 py-2 bg-gray-300 rounded disabled:opacity-50 disabled:cursor-no-drop cursor-pointer"
               >
-                {category.name}
+                Anterior
               </button>
-            ))}
-          </div>
-        )}
-      </div>
 
-       
-      <div className="flex justify-center mt-12 min-h-[200px]">
-        {loadingProducts ? (
-          <LoadingIcon />
-        ) : products.length === 0 ? (
-          <p className="text-gray-500 text-lg">Nenhum produto encontrado nessa categoria.</p>
-        ) : (
-          <ul className="grid grid-cols-4 gap-20">
-            {products.map((product: any) => (
-              <BoxProduct
-                key={product.id}
-                id={product.id}
-                img={product.Image}
-                name={product.ProductLine.name}
-              />
-            ))}
-          </ul>
-        )}
-      </div>
+              <span className="px-5">
+                Página <strong>{page}</strong> de{" "}
+                <strong>{pagination.totalPages}</strong>
+              </span>
 
-      <div className="flex justify-center items-center mt-10 gap-4">
-        {products.length > 0 && (
-          <div>
-            <button
-              disabled={page === 1}
-              onClick={() => setPage(page - 1)}
-              className="px-4 py-2 bg-gray-300 rounded disabled:opacity-50 disabled:cursor-no-drop cursor-pointer"
-            >
-              Anterior
-            </button>
-
-            <span className="px-5">
-              Página <strong>{page}</strong> de{" "}
-              <strong>{pagination.totalPages}</strong>
-            </span>
-
-            <button
-              disabled={page === pagination.totalPages}
-              onClick={() => setPage(page + 1)}
-              className="px-4 py-2 bg-gray-300 rounded disabled:opacity-50 disabled:cursor-no-drop cursor-pointer"
-            >
-              Próxima
-            </button>
-          </div>
-        )}
+              <button
+                disabled={page === pagination.totalPages}
+                onClick={() => setPage(page + 1)}
+                className="px-4 py-2 bg-gray-300 rounded disabled:opacity-50 disabled:cursor-no-drop cursor-pointer"
+              >
+                Próxima
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
