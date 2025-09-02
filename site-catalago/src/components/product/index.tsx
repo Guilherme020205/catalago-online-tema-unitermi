674449b90b73ed0f3a2170b5d4676f871bce5258
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2025-09-02 12:27:00 -0300

    Update index.tsx

--- site-catalago/src/components/product/index.tsx
+++ site-catalago/src/components/product/index.tsx
@@ -16,15 +16,15 @@ function BoxProduct({ id, img, name }: Product) {
   return (
     <div
       onClick={goToProduct}
-      className="w-48 h-64 cursor-pointer rounded-2xl shadow-[2px_2px_0px_-1px_#d0d0d0] hover:shadow-[6px_6px_0px_-1px_#d0d0d0] duration-700 transition-all p-2 bg-white"
+      className="w-48 h-56 p-2 cursor-pointer rounded-2xl shadow-[2px_2px_0px_-1px_#d0d0d0] hover:shadow-[6px_6px_0px_-1px_#d0d0d0] duration-700 transition-all bg-gray-50"
     >
       <div className="flex flex-col items-center h-full">
         <img
           src={img}
           alt={name}
-          className="w-full h-40 object-contain rounded-lg"
+          className="w-full h-40 object-cover rounded-lg"
         />
-        <h1 className="mt-2 text-center text-sm font-bold text-gray-800 line-clamp-2">
+        <h1 className="mt-4 text-center text-sm font-bold text-gray-800 line-clamp-1">
           {name}
         </h1>
       </div>
