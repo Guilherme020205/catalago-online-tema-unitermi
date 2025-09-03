22de86b66ad76a43c1c99bb8e60953d0ed2e34de
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2025-09-03 11:33:17 -0300

    Update index.tsx

--- site-cadastro/src/components/header/index.tsx
+++ site-cadastro/src/components/header/index.tsx
@@ -15,14 +15,14 @@ function Header() {
   };
 
   return (
-    <div className="flex bg-web-gray justify-between items-center px-10 py-1 select-none">
-      <img src={logo} alt="logo" className="w-36 cursor-pointer" onClick={() => navigate("/home")} />
+    <div className="flex bg-web-gray justify-between items-center px-2 md:px-10 py-2 select-none">
+      <img src={logo} alt="logo" className="w-20 md:w-32 cursor-pointer" onClick={() => navigate("/home")} />
       <button
         onClick={handleLogout}
         className="flex flex-row items-center gap-1 text-web-red hover:text-red-600 transition-all duration-700  cursor-pointer"
       >
         <RiLogoutCircleFill className="text-3xl" />
-        <p className="text-2xl">Sair do sistema</p>
+        <p className="text-xs md:text-xl">Sair do <p>sistema</p></p>
       </button>
     </div>
   );
